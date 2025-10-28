b7ffea8f443d1e3cf30a4ebd71efded91efdcf45
Ana <anitalima3228@gmail.com>
2025-10-28 17:12:32 -0300

    Adicionando testes

--- tests/test.js
+++ tests/test.js
@@ -10,8 +10,17 @@ app.use(cors());
 app.use(bodyParser.json());
 
 let alimentos = [
-  { id: '1', nome: 'Arroz', quantidade: 5, unidade: 'kg' },
-  { id: '2', nome: 'Feijão', quantidade: 2, unidade: 'kg' }
+  { id: '1', nome: 'Arroz', quantidade: 10, unidade: 'kg' },
+  { id: '2', nome: 'Feijão', quantidade: 10, unidade: 'kg' },
+  { id: '3', nome: 'Macarrão', quantidade: 10, unidade: 'kg' },
+  { id: '4', nome: 'Farinha', quantidade: 10, unidade: 'kg' },
+  { id: '5', nome: 'Flocão', quantidade: 10, unidade: 'kg' },
+  { id: '6', nome: 'Bolacha', quantidade: 10, unidade: 'Unidade' },
+  { id: '7', nome: 'Trigo', quantidade: 10, unidade: 'kg' },
+  { id: '8', nome: 'Margarina', quantidade: 10, unidade: 'kg' },
+  { id: '9', nome: 'Ovos', quantidade: 10, unidade: 'Unidade' },
+  { id: '10', nome: 'Café', quantidade: 10, unidade: 'kg' },
+
 ];
 
 app.get('/alimentos', (req, res) => res.json(alimentos));
